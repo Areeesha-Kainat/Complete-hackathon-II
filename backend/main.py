0fed0bb89f63ef80832f571c9be9222271902221
Areesha Kainat <133228317+Areeesha-Kainat@users.noreply.github.com>
2026-02-09 00:00:45 +0500

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+# main.py
+from fastapi import FastAPI
+from routes import router
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(title="Todo App with Auth")
+
+# Allow frontend to talk
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "Backend is running 🚀"}
